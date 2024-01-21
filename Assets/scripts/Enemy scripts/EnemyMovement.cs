2b2cf4e12fcbc76bd613f9102bfb2bf9a2adc699
rrdpad <63047528+rrdpad@users.noreply.github.com>
2024-01-21 17:18:09 +0300

    бан

--- Assets/scripts/Enemy scripts/EnemyMovement.cs
+++ Assets/scripts/Enemy scripts/EnemyMovement.cs
@@ -9,58 +9,31 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed;
 
+    private Transform _player;
+
     private Rigidbody2D _rigidbody;
-    private PlayerAwarenessController _playerAwarenessController;
-    private Vector2 _targetedDirection;
+    private Vector3 v_diff;
+    private float atan2;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        _player = FindObjectOfType<RotateClass>().transform;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        UpdateTargetDirection();
-        RotateTowardsTarget();
-        SetVelocity();
-    }
+        Rotation();
+        //TODO: ДАБАВИТЬ ДВИЖЕНИЕ
+        _rigidbody.velocity = new Vector2(1,0);
 
-    private void UpdateTargetDirection()
+    }
+    private void Rotation()
     {
-        if (_playerAwarenessController.AwareOfPlayer)
-        {
-            _targetedDirection = _playerAwarenessController.DirectionToPlayer;
-        }
-        else
-        {
-            _targetedDirection = Vector2.zero;
-        }
+        v_diff = (_player.position - transform.position);
+        atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
+        transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg - 90f);
     }
 
-    private void RotateTowardsTarget()
-    {
-        if (_targetedDirection == Vector2.zero)
-        {
-            return;
-        }
-
-        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetedDirection);
-        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
-
-        _rigidbody.SetRotation(rotation);
-    }
-
-    private void SetVelocity()
-    {
-        if (_targetedDirection == Vector2.zero)
-        {
-            _rigidbody.velocity = Vector2.zero;
-        }
-        else
-        {
-            _rigidbody.velocity = transform.up * _speed;
-        }
-    }
 }
