7bdb73c06f0bdf9a55ea75663a7130b9c01e73d4
asdopk1 <ptichka.denozavra05@mail.ru>
2024-01-29 19:49:36 +0700

    22

--- Assets/scripts/test/score.cs
+++ Assets/scripts/test/score.cs
@@ -9,11 +9,15 @@ public class score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void AddPoint()
     {
         
     }
